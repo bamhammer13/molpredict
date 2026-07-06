b0dea9680e1220749995599d0f83e1eac9f9ee86
bamhammer13 <wil.howes@icloud.com>
2026-07-05 22:24:06 -0400

    Add front-end javaScript to hook up the HTML to /predict

--- static/app.js
+++ static/app.js
@@ -1,1 +1,18 @@
-console.log("app.js loaded");
+async function runPrediction() { // Function used by the html to get the prediction results, async to allow use of await
+    //Uses getElementById to scour the HTML
+    const smiles = document.getElementById("smiles-input").value; // Reads in the input typed by the user
+    
+    const resultEl = document.getElementById("result"); // Finds the area we will write the result to 
+    resultEl.textContent = "Predicting..."; // Just lets the user know that progress is happening
+
+    // Sends in the molecule into the API for prediction, and stores the result
+    const response = await fetch("/predict", {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ smiles: smiles})
+    });
+
+    const data = await response.json(); // Turns the JSON response into a JS object, uses await to let other things happen while waiting
+
+    resultEl.textContent = `Predicted logS: ${data.logS.toFixed(3)} (higher means more soluble in water)`; // Puts the results on screen
+}
